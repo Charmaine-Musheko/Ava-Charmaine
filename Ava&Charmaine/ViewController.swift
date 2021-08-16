66069d4330f67325e891196a170d3ae6e627cead
Charmaine-Musheko <67749385+Charmaine-Musheko@users.noreply.github.com>
2021-08-16 16:19:02 +0200

    Initial Commit

---
+++ Ava&Charmaine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ava&Charmaine
+//
+//  Created by Charmaine Musheko on 16/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
