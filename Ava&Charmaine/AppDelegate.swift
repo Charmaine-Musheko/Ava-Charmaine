340e072ca8a9dbc164eb7f462ed9d08bc82bb145
Charmaine-Musheko <67749385+Charmaine-Musheko@users.noreply.github.com>
2021-08-16 17:07:00 +0200

    init

--- Ava&Charmaine/AppDelegate.swift
+++ Ava&Charmaine/AppDelegate.swift
@@ -1,12 +1,16 @@
 //
 //  AppDelegate.swift
-//  Ava&Charmaine
+//  Ava&Charmaine1
 //
 //  Created by Charmaine Musheko on 16/08/2021.
 //
 
 import UIKit
 import CoreData
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let Context = appDelegate.persistentContainer.viewContext
+let Save = appDelegate.saveContext()
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -41,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "Ava_Charmaine")
+        let container = NSPersistentContainer(name: "Ava_Charmaine1")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
